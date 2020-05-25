3f2076d353dc5c68c9ace7d03610ea2346042b66
Vishesh jindal <46999893+jindal368@users.noreply.github.com>
2020-05-25 16:17:37 +0530

    Update worker.js

--- client/worker.js
+++ client/worker.js
@@ -5,7 +5,7 @@ self.addEventListener('push', e =>{
     const data = e.data.json();
     console.log('push recieved...');
     self.ServiceWorkerRegistration.showNotification(data.title,{
-        body:'Notified by vishesh!',
-        icon:'http://image.ibb.co/frYOFd/tmlogo.png'
+        body:'Notified by vishesh!'
+        
     });
 });
